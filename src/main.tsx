ebff108638126a84dedc1682b836dab9094a7ec8
Masaya Ido <alice-96@Stradas.local>
2022-04-10 08:04:21 +0900

    feature: add routing at App and refer App at Main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import YupTrial from './YupTrial';
-import YupTrialNested from './YupTrialNested';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <YupTrial /> */}
-    <YupTrialNested />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
